fb8a9b4b277157d41ca562375ff61bb21429a729
Pieter De Praetere <pieter@packed.be>
2017-01-09 10:20:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Catmandu::VIAF - Blah blah blah
+
+# SYNOPSIS
+
+    use Catmandu::VIAF;
+
+# DESCRIPTION
+
+Catmandu::VIAF is
+
+# AUTHOR
+
+Pieter De Praetere <pieter.de.praetere@helptux.be>
+
+# COPYRIGHT
+
+Copyright 2017- Pieter De Praetere
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Catmandu/VIAF.pm
@@ -0,0 +1,39 @@
+package Catmandu::VIAF;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Catmandu::VIAF - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Catmandu::VIAF;
+
+=head1 DESCRIPTION
+
+Catmandu::VIAF is
+
+=head1 AUTHOR
+
+Pieter De Praetere E<lt>pieter.de.praetere@helptux.beE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2017- Pieter De Praetere
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
